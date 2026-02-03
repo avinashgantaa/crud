c53c858561fe4ae353a94892a935e604f065df8e
avinashgantaa <avinash.ganta39@gmail.com>
2026-02-03 15:47:56 +0530

    add

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,8 +1,8 @@
-import { Component, inject,OnInit } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
-import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms'
+import { Component, inject, OnInit } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Auth, LoginPayload } from '../../service/auth';
 
 @Component({
@@ -12,27 +12,36 @@ import { Auth, LoginPayload } from '../../service/auth';
   styleUrl: './login.css',
 })
 export class Login implements OnInit {
-  fb=inject(FormBuilder)
-  service=inject(Auth)
-  loginForm!:FormGroup
+  fb = inject(FormBuilder);
+  service = inject(Auth);
+  loginForm!: FormGroup;
+  allusers!: LoginPayload[];
   ngOnInit(): void {
-    this.loginForm=this.fb.group({
-      username:[''],
-      password:['']
-    })
-
+    this.loginForm = this.fb.group({
+      username: [''],
+      password: [''],
+    });
   }
-  login(){
-    console.log(this.loginForm.value);
-    this.service.login(this.loginForm.value).subscribe({
-      next:(res)=>{
-        console.log(res);
+  login() {
+    this.service.allusers().subscribe({
+      next: (res) => {
+        this.allusers = res;
+        console.log(this.allusers);
+        console.log(this.loginForm.value);
+        this.allusers.forEach((user: LoginPayload) => {
+          if (user.username.toLowerCase() === this.loginForm.value.username.toLowerCase()) {
+            console.log('user found');
+            return 'login successful';
+          }
+          else{
+            console.log('user not found');
+            return 'login failed';
+          }
+        });
       },
-      error:(err)=>{
+      error: (err) => {
         console.log(err);
-      }
-    })
-    
+      },
+    });
   }
-
 }
--- src/app/service/auth.ts
+++ src/app/service/auth.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 export interface LoginPayload {
   username: string;
   password: string;
@@ -12,5 +12,9 @@ export class Auth {
   http = inject(HttpClient);
   login(payload: LoginPayload): Observable<string> {
     return this.http.post<string>('http://localhost:3000/users', payload);
+  }
+
+  allusers(): Observable<LoginPayload[]> {
+    return this.http.get<LoginPayload[]>('http://localhost:3000/users');
   }
 }
