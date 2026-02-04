9bc03e7b799673e6705a17193b26081ff6d150d0
avinashgantaa <avinash.ganta39@gmail.com>
2026-02-04 15:08:45 +0530

    first change

--- src/app/services/parent-guard.ts
+++ src/app/services/parent-guard.ts
@@ -1,11 +1,9 @@
 import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { Logic } from './logic';
-import {Router} from '@angular/router';
 
 export const parentGuard: CanActivateFn = (route, state) => {
   const logicService = inject(Logic);
-  const router = inject(Router);
   if(logicService.loginsuccess()){ 
     return true;
   }
